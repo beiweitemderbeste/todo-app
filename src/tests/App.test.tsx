b00c63e53237e507bbe48ed563a1c75594386fd7
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-08-16 18:46:01 +0200

    adding a button test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,9 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
-describe('Simple working test', () => {
-  it('the title is visible', () => {
-    render(<App />)
-    expect(screen.getByText(/React Todo App/i)).toBeInTheDocument()
-  })
-})
+it("tests if the title is visible", () => {
+  render(<App />);
+  expect(screen.getByText(/React Todo App/i)).toBeInTheDocument();
+});
---
+++ src/tests/InputForm.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import InputForm from "../components/InputForm";
+import jest from "jest-mock";
+import { InputFormProps } from "../ts/interfaces/InputForm.interfaces";
+import { TodoItem } from "../ts/interfaces/App.interfaces";
+
+it("tests if the button is there", () => {
+  const mockSetTodos = jest.fn();
+  const mockTodos: TodoItem[] = [];
+
+  const inputFormProps: InputFormProps = {
+    todos: mockTodos,
+    setTodos: mockSetTodos,
+  };
+
+  render(<InputForm {...inputFormProps} />);
+  expect(screen.getByRole("button")).toBeInTheDocument();
+});
