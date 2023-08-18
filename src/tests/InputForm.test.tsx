ff0b6c3c62149f711d23a6b78d1812bcaf20352e
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-08-18 17:29:09 +0200

    adding test for the input field

--- src/tests/InputForm.test.tsx
+++ src/tests/InputForm.test.tsx
@@ -1,10 +1,24 @@
 import { render, screen } from "@testing-library/react";
-import InputForm from "../components/InputForm";
 import jest from "jest-mock";
+import userEvent from "@testing-library/user-event";
+import InputForm from "../components/InputForm";
 import { InputFormProps } from "../ts/interfaces/InputForm.interfaces";
 import { TodoItem } from "../ts/interfaces/App.interfaces";
 
-it("tests if the button is there", () => {
+it("tests if the inputfield is there", () => {
+  const mockSetTodos = jest.fn();
+  const mockTodos: TodoItem[] = [];
+
+  const inputFormProps: InputFormProps = {
+    todos: mockTodos,
+    setTodos: mockSetTodos,
+  };
+
+  render(<InputForm {...inputFormProps} />);
+  expect(screen.getByPlaceholderText("Enter Something")).toBeInTheDocument();
+});
+
+it("tests if the input button is there", () => {
   const mockSetTodos = jest.fn();
   const mockTodos: TodoItem[] = [];
 
@@ -16,4 +30,3 @@ it("tests if the button is there", () => {
   render(<InputForm {...inputFormProps} />);
   expect(screen.getByRole("button")).toBeInTheDocument();
 });
-
