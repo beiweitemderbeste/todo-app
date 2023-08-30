8a17919278029f5be39607d63fff32438ec54715
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-08-30 18:05:57 +0200

    separating client and server

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     css: true,
-    setupFiles: "./src/tests/setup.ts",
+    setupFiles: "src/tests/setup.ts",
   },
 });
