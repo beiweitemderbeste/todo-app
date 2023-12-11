6430314080935a29d826b3250b94d254c521565a
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-12-11 19:40:50 +0100

    removing unnessecary tests

--- src/tests/App.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "../App";
-
-it("tests if the title is visible", () => {
-  render(<App />);
-  expect(screen.getByText(/React Todo App/i)).toBeInTheDocument();
-});
