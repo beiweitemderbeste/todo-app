277ce09579f8a6d29ca48f3d0968b5c60906d519
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-12-18 17:24:23 +0100

    adjusted test to the changed components

--- src/components/TodoList/TodoListHeading.test.tsx
+++ src/components/TodoList/TodoListHeading.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import TodoListHeading from './TodoListHeading';
 
 test('renders Todo List heading', () => {
-  render(<TodoListHeading />);
-  const headingElement = screen.getByText(/Todo List/i);
+  render(<TodoListHeading tableHeading="Test Heading" />);
+  const headingElement = screen.getByText(/Test Heading/i);
   expect(headingElement).toBeInTheDocument();
 });
