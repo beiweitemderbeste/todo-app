12d6fa2f5e92f2ae3552c9812e485b5f3cdaaec2
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-12-22 23:08:47 +0100

    updating types

--- src/pages/ReTodo/ReTodo.tsx
+++ src/pages/ReTodo/ReTodo.tsx
@@ -3,17 +3,19 @@ import { useState } from "react";
 import Navbar from "../../components/layout/navigation/Navbar/Navbar";
 import ReTodoListTable from "../../components/ReTodoList/ReTodoListTable";
 
+import type { TodoItem } from "../../ts/interfaces/TodoItem.interface";
+
 function ReTodo() {
-  const [dailyReTodos, setDailyReTodos] = useState([
+  const [dailyReTodos, setDailyReTodos] = useState<TodoItem[]>([
     { id: "1", title: "remove trash", status: true },
     { id: "2", title: "do dishes", status: false },
   ]);
 
-  const [weeklyReTodos, setWeeklyReTodos] = useState([
+  const [weeklyReTodos, setWeeklyReTodos] = useState<TodoItem[]>([
     { id: "1", title: "water plants", status: false },
   ]);
 
-  const [monthlyReTodos, setMonthlyReTodos] = useState([
+  const [monthlyReTodos, setMonthlyReTodos] = useState<TodoItem[]>([
     { id: "1", title: "pay bills", status: false },
   ]);
 
