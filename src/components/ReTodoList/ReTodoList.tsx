b30c10642fd5362c668ad61dd1bd92d07e27a56b
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2023-12-22 20:47:09 +0100

    adding checkboxchange

--- src/components/ReTodoList/ReTodoList.tsx
+++ src/components/ReTodoList/ReTodoList.tsx
@@ -2,16 +2,56 @@ import { useState } from "react";
 
 export default function ReTodoList() {
   const [dailyReTodos, setDailyReTodos] = useState([
-    { id: "1", title: "trash", status: false },
+    { id: "1", title: "trash", status: true },
     { id: "2", title: "plants", status: false },
   ]);
+
+  const handleCheckboxChange = (retodo: string) => {
+    console.log(retodo);
+    setDailyReTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === retodo ? { ...todo, status: !todo.status } : todo
+      )
+    );
+  };
+
+  const handleDelete = async (todoId: string) => {
+    setDailyReTodos((prevTodos) =>
+      prevTodos.filter((item) => item.id !== todoId)
+    );
+  };
 
   return (
     <>
       <ul className="divide-y divide-gray-200 px-4">
         {dailyReTodos.map((retodo) => (
           <li className="py-4 flex justify-center h-14" key={retodo.id}>
-            <span className="text-lg font-medium">{retodo.title}</span>
+            <div className="flex items-center w-full">
+              <input
+                id={retodo.id}
+                name={retodo.id}
+                type="checkbox"
+                checked={retodo.status}
+                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                onChange={() => handleCheckboxChange(retodo.id)}
+              />
+              <label
+                htmlFor={retodo.id}
+                className={`ml-3 block text-gray-900 ${
+                  retodo.status ? "line-through text-gray-500" : ""
+                }`}
+              >
+                <span className="text-lg font-medium">{retodo.title}</span>
+              </label>
+              {retodo.status && (
+                <button
+                  className="ml-auto text-white bg-red-500 px-2 py-1 rounded"
+                  onClick={() => handleDelete(retodo.id)}
+                >
+                  Delete
+                </button>
+              )}
+            </div>
           </li>
         ))}
       </ul>
--- src/components/ReTodoList/ReTodoListHeading.tsx
+++ src/components/ReTodoList/ReTodoListHeading.tsx
@@ -1,7 +1,7 @@
 export default function ReTodoListHeading() {
   return (
     <div className="p-4 bg-indigo-500 text-white">
-      <h2 className="text-2xl font-bold">Daily Todos</h2>
+      <h2 className="text-2xl font-bold">Daily ReTodos</h2>
     </div>
   )
 }
