6305b9585155cc52c8352ec1cb67707ef26cdb0f
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-01-05 23:03:31 +0100

    adding login and register page files

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/layout/navigation/Navbar/Navbar";
+
+const Login: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>hello from Login page</h1>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register/Register.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/layout/navigation/Navbar/Navbar";
+
+const Register: React.FC = () => {
+  return (
+    <>
+      <Navbar />
+      <h1>hello from register page</h1>
+    </>
+  );
+};
+
+export default Register;
