3a69313a952d3b50b47e1112979573dc731e2f39
beiweitemderbeste <beiweitemderbeste@protonmail.com>
2024-01-08 01:18:26 +0100

    showing buttons depending on login status

--- src/components/layout/navigation/Navbar/Navbar.tsx
+++ src/components/layout/navigation/Navbar/Navbar.tsx
@@ -6,13 +6,11 @@ import LogoutButton from "../../../LogoutButton/LogoutButton";
 import type { NavbarProps } from "../../../../ts/interfaces/Navbar/NavbarProps";
 
 const Navbar: React.FC<NavbarProps> = ({ session }) => {
-  
-
   return (
     <nav className="relative flex items-center justify-between sm:h-10 md:justify-center py-6 px-4 mt-2">
       <BrandLogo />
       <LinkList />
-      <SigningButtons />
+      {!session ? <SigningButtons /> : <LogoutButton />}
     </nav>
   );
 };
